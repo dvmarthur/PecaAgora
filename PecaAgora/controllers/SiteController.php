f0bdd30e10d27c99040b96b3f94d852b48a2689b
dvmarthur <83983660+dvmarthur@users.noreply.github.com>
2022-04-17 17:21:16 -0300

    melhorando o frontend+ideias novas

--- PecaAgora/controllers/SiteController.php
+++ PecaAgora/controllers/SiteController.php
@@ -3,47 +3,11 @@
 namespace app\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
 
 class SiteController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
 
-                    ],
-                ],
-            ],
-
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function actions()
     {
         return [
@@ -62,57 +26,15 @@ class SiteController extends Controller
         return parent::beforeAction($action);
     }
 
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
     public function actionIndex()
     {
-
-
         return $this->render('index');
-    }
-
-    /**
-     * Login action.
-     *
-     * @return Response|string
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-
-        $model->password = '';
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return Response
-     */
-    public function actionLogout()
-    {
-        die(Var_dump("foi"));
-        Yii::$app->user->logout();
-
-        return $this->goHome();
     }
 
     public function actionObter()
     {
-        $consulta =[];    
-       
+        $consulta = [];
+
         if (!empty($_POST['meli_id'])) {
 
             $id = str_replace('-', '', $_POST['meli_id']);
@@ -135,10 +57,10 @@ class SiteController extends Controller
 
             curl_close($curl);
 
-           if(isset($response['error'])){
+            if (isset($response['error'])) {
                 Yii::$app->session->setFlash('error', 'Produto não encontrado!');
                 return $this->render('index', $consulta);
-           }
+            }
 
             //buscando categoria pelo categoria ID
             $curl = curl_init();
@@ -159,9 +81,9 @@ class SiteController extends Controller
             $categoria_nome = json_decode(curl_exec($curl), true)['name'];
 
             curl_close($curl);
-        
+
             // end session categoria 
-            
+
             $consulta = [
                 'id' => $response['id'],
                 'title' => $response['title'],
@@ -175,33 +97,5 @@ class SiteController extends Controller
 
         Yii::$app->session->setFlash('success', 'Consulta realizada com sucesso!');
         return $this->render('index', $consulta);
-    }
-
-    /**
-     * Displays contact page.
-     *
-     * @return Response|string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
     }
 }
