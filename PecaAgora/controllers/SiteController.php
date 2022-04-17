0abe596c8167d8ca019827b28371b31a618f4626
dvmarthur <83983660+dvmarthur@users.noreply.github.com>
2022-04-17 16:49:41 -0300

    Consultando e dando mensagens de erro

--- PecaAgora/controllers/SiteController.php
+++ PecaAgora/controllers/SiteController.php
@@ -26,13 +26,16 @@ class SiteController extends Controller
                         'actions' => ['logout'],
                         'allow' => true,
                         'roles' => ['@'],
+
                     ],
                 ],
             ],
+
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
                     'logout' => ['post'],
+
                 ],
             ],
         ];
@@ -53,6 +56,11 @@ class SiteController extends Controller
             ],
         ];
     }
+    public function beforeAction($action)
+    {
+        $this->enableCsrfValidation = false;
+        return parent::beforeAction($action);
+    }
 
     /**
      * Displays homepage.
@@ -61,6 +69,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
+
+
         return $this->render('index');
     }
 
@@ -93,9 +103,78 @@ class SiteController extends Controller
      */
     public function actionLogout()
     {
+        die(Var_dump("foi"));
         Yii::$app->user->logout();
 
         return $this->goHome();
+    }
+
+    public function actionObter()
+    {
+        $consulta =[];    
+       
+        if (!empty($_POST['meli_id'])) {
+
+            $id = str_replace('-', '', $_POST['meli_id']);
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.mercadolibre.com/items/{$id}",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
+
+            $response = json_decode(curl_exec($curl), true);
+
+            curl_close($curl);
+
+           if(isset($response['error'])){
+                Yii::$app->session->setFlash('error', 'Produto não encontrado!');
+                return $this->render('index', $consulta);
+           }
+
+            //buscando categoria pelo categoria ID
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.mercadolibre.com/categories/{$response['category_id']}",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
+
+            $categoria_nome = json_decode(curl_exec($curl), true)['name'];
+
+            curl_close($curl);
+        
+            // end session categoria 
+            
+            $consulta = [
+                'id' => $response['id'],
+                'title' => $response['title'],
+                'price' => $response['price'],
+                'categoria' => $categoria_nome,
+                'available_quantity' => $response['available_quantity'],
+                'thumbnail' => $response['thumbnail'],
+                'permalink' => $response['permalink']
+            ];
+        }
+
+        Yii::$app->session->setFlash('success', 'Consulta realizada com sucesso!');
+        return $this->render('index', $consulta);
     }
 
     /**
--- PecaAgora/views/site/index.php
+++ PecaAgora/views/site/index.php
@@ -3,51 +3,46 @@
 /** @var yii\web\View $this */
 
 $this->title = 'My Yii Application';
+
 ?>
 <div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+    <form action="/index.php?r=site/obter" method="POST">
+        <div class="jumbotron text-center bg-transparent">
+            <h1 class="display-4">Bem-vindo!</h1>
+            <p class="lead">Digite o meli_id do produto que deseja consultar!</p>
+            <input type="text" class="btn btn-lg btn-success" name="meli_id">
+            <input type="submit" class="btn btn-lg btn-success" name="envia">
         </div>
+    </form>
+</div>
 
+<div class="body-content">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">available quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if(!empty($id)){ ?>
+                    <tr>
+                        <th scope="row"><?php echo $id ?></th>
+                        <td><?php echo $title ?></td>
+                        <td><?php echo $categoria ?></td>
+                        <td>R$<?php echo number_format($price,2,",","."); ?></td>
+                        <td><?php echo $available_quantity ?></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
+
 </div>
