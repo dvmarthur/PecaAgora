6b1c757e967271848dc9b2377a96c1a7163853d4
DESKTOP-1FI7BA5\Arthur <arthurdvm2@gmail.com>
2022-04-17 21:04:17 -0300

    finalizado

--- PecaAgora/controllers/SiteController.php
+++ PecaAgora/controllers/SiteController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use yii\web\Controller;
 
+
 class SiteController extends Controller
 {
 
@@ -26,6 +27,28 @@ class SiteController extends Controller
         return parent::beforeAction($action);
     }
 
+    function request_get($url, $id)
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "{$url}{$id}",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        curl_close($curl);
+        return json_decode(curl_exec($curl), true);
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
@@ -38,64 +61,29 @@ class SiteController extends Controller
         if (!empty($_POST['meli_id'])) {
 
             $id = str_replace('-', '', $_POST['meli_id']);
-            $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api.mercadolibre.com/items/{$id}",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_SSL_VERIFYPEER => false,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-            ));
-
-            $response = json_decode(curl_exec($curl), true);
-
-            curl_close($curl);
+            $response = $this->request_get('https://api.mercadolibre.com/items/', $id);
 
             if (isset($response['error'])) {
                 Yii::$app->session->setFlash('error', 'Produto não encontrado!');
                 return $this->render('index', $consulta);
             }
 
-            //buscando categoria pelo categoria ID
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api.mercadolibre.com/categories/{$response['category_id']}",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_SSL_VERIFYPEER => false,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-            ));
-
-            $categoria_nome = json_decode(curl_exec($curl), true)['name'];
-
-            curl_close($curl);
-
-            // end session categoria 
+            $categoria_nome = $this->request_get('https://api.mercadolibre.com/categories/', $response['category_id']);
 
             $consulta = [
                 'id' => $response['id'],
                 'title' => $response['title'],
                 'price' => $response['price'],
-                'categoria' => $categoria_nome,
+                'categoria' => $categoria_nome['name'],
                 'available_quantity' => $response['available_quantity'],
                 'thumbnail' => $response['thumbnail'],
                 'permalink' => $response['permalink']
             ];
+            Yii::$app->session->setFlash('success', 'Consulta realizada com sucesso!');
+        } else {
+            Yii::$app->session->setFlash('warning', 'Campo não pode ser vazio');
         }
-
-        Yii::$app->session->setFlash('success', 'Consulta realizada com sucesso!');
         return $this->render('index', $consulta);
     }
 }
